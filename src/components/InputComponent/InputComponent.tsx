54a8b3c45b97fa21dcecd63e695b43c06001c951
Ejim Favour <favourejim56@gmail.com>
2022-12-24 16:42:56 +0100

    THULLO-37-Implement-sign-up-page: created reusable input component

---
+++ src/components/InputComponent/InputComponent.tsx
@@ -0,0 +1,32 @@
+const InputComponent: React.FC<{
+  placeholder: string;
+  type: string;
+  name: string;
+  validation: {};
+  register: any;
+  error: any;
+  icon: any;
+}> = (props) => {
+  const error: string = props.error[props.name]?.message
+    ? "text-color-red border-color-red"
+    : "border-color-border text-text-p-color";
+
+  return (
+    <div className="flex flex-col mb-3.5">
+      <div className="flex relative w-full">
+        <input
+          type={props.type}
+          placeholder={props.placeholder}
+          {...props.register(props.name, props.validation)}
+          className={`border  py-2 pl-11 pr-3 w-full rounded-lg outline-0 ${error}`}
+        />
+        {props.icon}
+      </div>
+      <small className="text-color-red pt-1">
+        {props.error[props.name]?.message}
+      </small>
+    </div>
+  );
+};
+
+export default InputComponent;
