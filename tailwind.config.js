e70fe3283b0181dffd8cacd38b66df7c5b10b66f
Ejim Favour <favourejim56@gmail.com>
2022-12-24 16:38:33 +0100

    THULLO-37-Implement-sign-up-page: configured taailwind file for media query and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      "color-border": "#BDBDBD",
+      "text-p-color": "#333333",
+      "color-red": "rgb(220 38 38)",
+      "color-white": "#fff",
+      "color-btn": "#2F80ED",
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "850px" },
+      // => @media (max-width: 850px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {},
   },
   plugins: [],
-}
+};
