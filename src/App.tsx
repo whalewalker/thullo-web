0a516ca5ccdec09fb55785ffc309d3ef7770d875
Ejim Favour <favourejim56@gmail.com>
2022-12-27 00:08:20 +0100

    THULLO-39-Implement-the-login-page: added the login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./Pages/auth/SignUp";
+import Login from "./Pages/auth/Login";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
