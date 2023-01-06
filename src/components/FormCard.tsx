57b4e794059bda1e479c26e905e27cc0acdacb90
whalewalker <mongodbms@gmail.com>
2023-01-06 09:23:12 +0100

    created edit for for user profile

--- src/components/FormCard.tsx
+++ src/components/FormCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const FormCard: React.FC<{ children: React.ReactNode }> = ({children}) => {
+const FormCard: React.FC<any> = (props) => {
     return (
         <section className="flex justify-center items-center py-5 min-h-screen ">
-            <div className="w-1/3 border border-color-border rounded-3xl px-10 py-7 sm:w-full sm:border-0  lg:p-6 md:w-3/5 lg:w-1/2 xl:w-2/5">
-                {children}
+            <div className={`${props.width ? props.width : "w-1/3"} border border-color-border rounded-3xl px-10 py-7 sm:w-full sm:border-0  lg:p-6 md:w-3/5 lg:w-1/2 xl:w-2/5`}>
+                {props.children}
             </div>
         </section>
     )
