5baf0192e2f4a6b16be4383f229604a7ab2d2dc8
Ejim Favour <favourejim56@gmail.com>
2023-01-12 05:55:29 +0100

    implement-all-boards-section: added the allboards page and single board route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import {Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/auth/SignUp";
 import Login from "./pages/auth/Login";
 import ForgotPassword from "./pages/auth/ForgotPassword";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ResetPassword from "./pages/auth/ResetPassword";
 import Dashboard from "./pages/user//Dashboard";
@@ -14,31 +14,32 @@ import ProfileEdit from "./pages/user/ProfileEdit";
 import ProfileMain from "./pages/user/ProfileMain";
 import ProtectRoute from "./components/ProtectRoute";
 import PageNotFound from "./components/PageNotFound";
+import BoardDetail from "./components/BoardDetail";
 
 const App = () => {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Navigate to={"/user/dashboard"}/>}/>
-                <Route path="sign-up" element={<SignUp/>}/>
-                <Route path="login" element={<Login/>}/>
-                <Route path="forgot-password" element={<ForgotPassword/>}/>
-                <Route path="reset-password" element={<ResetPassword/>}/>
-                <Route path="user" element={
-                    <ProtectRoute><User/></ProtectRoute>}>
-                    <Route path="" element={<Dashboard/>}>
-                        <Route path="" element={<Navigate to={"dashboard"}/>}/>
-                        <Route path="dashboard" element={<DashboardMain/>}/>
-                    </Route>
-                    <Route path="profile" element={<Profile/>}>
-                        <Route path="" element={<ProfileMain/>}/>
-                        <Route path="edit" element={<ProfileEdit/>}/>
-                    </Route>
-                </Route>
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-            <ToastContainer limit={1}/>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to={"/user/dashboard"} />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="user" element={<User />}>
+          <Route path="" element={<Dashboard />}>
+            <Route path="" element={<Navigate to={"dashboard"} />} />
+            <Route path="dashboard" element={<DashboardMain />} />
+          </Route>
+          <Route path="board/:boardId" element={<BoardDetail />} />
+          <Route path="profile" element={<Profile />}>
+            <Route path="" element={<ProfileMain />} />
+            <Route path="edit" element={<ProfileEdit />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <ToastContainer limit={1} />
+    </>
+  );
 };
 export default App;
