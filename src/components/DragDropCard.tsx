71fa1a7ad9fd302a91407c041dfb44dde2e300f8
Ejim Favour <favourejim56@gmail.com>
2023-01-30 21:03:51 +0100

    modified the card

--- src/components/DragDropCard.tsx
+++ src/components/DragDropCard.tsx
@@ -6,7 +6,6 @@ interface CardItem {
   img: string | undefined;
   cardTitle: string;
   cardId: string;
-  index: number;
   labels: { bgColor: string; textColor: string; text: string }[];
   collabs: string[];
 }
@@ -18,7 +17,7 @@ const isImage = (url: string) => {
 const DragDropCard = ({ card, index }: { card: CardItem; index: number }) => {
   return (
     <Draggable draggableId={card.cardId} index={index}>
-      {(provided, snapshot) => (
+      {(provided) => (
         <li
           ref={provided.innerRef}
           {...provided.draggableProps}
@@ -29,40 +28,44 @@ const DragDropCard = ({ card, index }: { card: CardItem; index: number }) => {
             <img
               src={card.img}
               alt={card.cardTitle}
-              className="rounded-lg mb-3"
+              className="rounded-lg mb-3 h-40 w-full"
             />
           )}
           <p className="text-[#000] font-normal mb-3">{card.cardTitle}</p>
-          <div className="flex items-center mb-4">
-            {card.labels.map((label, i) => (
-              <p
-                key={i}
-                className={`${label.bgColor} ${label.textColor} text-[10px] w-max px-2 py-1 mr-2 rounded-lg `}
-              >
-                {label.text}
-              </p>
-            ))}
-          </div>
-          <div className="flex items-center ">
-            {card.collabs.map((userAvatar, i) => {
-              return isImage(userAvatar) ? (
-                <img
-                  src={userAvatar}
-                  alt="collab-img"
-                  key={i}
-                  className={`w-8 h-8 mr-1 sm:w-9 sm:h-9 relative rounded-full ${
-                    i === 0 ? "z-20" : i === 1 ? "z-10" : "z-0"
-                  }`}
-                />
-              ) : (
+          {card.labels && (
+            <div className="flex items-center mb-4">
+              {card.labels.map((label, i) => (
                 <p
                   key={i}
-                  className="w-8 h-8 flex items-center justify-center bg-[#BDBDBD] mr-1 text-color-white rounded-lg text-xs"
+                  className={`${label.bgColor} ${label.textColor} text-[10px] w-max px-2 py-1 mr-2 rounded-lg `}
                 >
-                  {userAvatar.slice(0, 2).toUpperCase()}
+                  {label.text}
                 </p>
-              );
-            })}
+              ))}
+            </div>
+          )}
+
+          <div className="flex items-center ">
+            {card.collabs &&
+              card.collabs.map((userAvatar, i) => {
+                return isImage(userAvatar) ? (
+                  <img
+                    src={userAvatar}
+                    alt="collab-img"
+                    key={i}
+                    className={`w-8 h-8 mr-1 sm:w-9 sm:h-9 relative rounded-full ${
+                      i === 0 ? "z-20" : i === 1 ? "z-10" : "z-0"
+                    }`}
+                  />
+                ) : (
+                  <p
+                    key={i}
+                    className="w-8 h-8 flex items-center justify-center bg-[#BDBDBD] mr-1 text-color-white rounded-lg text-xs"
+                  >
+                    {userAvatar.slice(0, 2).toUpperCase()}
+                  </p>
+                );
+              })}
             <div className="w-8 h-8 flex items-center rounded-lg justify-center bg-color-btn ml-2 cursor-pointer">
               <BsPlusLg className="text-color-white " />
             </div>
