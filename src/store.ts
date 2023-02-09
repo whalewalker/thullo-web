c14b37a14e357fa8b75213ac818efc98abf3e30c
Ejim Favour <favourejim56@gmail.com>
2023-02-09 19:21:30 +0100

    removed dnd from the store

--- src/store.ts
+++ src/store.ts
@@ -4,14 +4,14 @@ import userReducer from "./slice/userSlice";
 import boardReducer from "./slice/boardSlice";
 import dragAndDropReducer from "./slice/dragAndDropSlice";
 
- const  store = configureStore({
-    reducer: {
-        auth: authReducer,
-        user: userReducer,
-        dragAndDrop: dragAndDropReducer,
-        board: boardReducer,
-    }
-})
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+    // dragAndDrop: dragAndDropReducer,
+    board: boardReducer,
+  },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
